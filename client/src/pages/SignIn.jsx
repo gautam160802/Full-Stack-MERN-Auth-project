012285763adbd3567ddc7e332ad8da4304b3fa6b
gautam kumar <120775495+gautam160802@users.noreply.github.com>
2025-02-21 12:19:12 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
 
-const SignIn = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null); // Define error state
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+
   const navigate = useNavigate();
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setLoading(true);
-    setError(null); // Reset error state
-
     try {
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -28,21 +28,17 @@ const SignIn = () => {
         },
         body: JSON.stringify(formData),
       });
-
       const data = await res.json();
-
-      if (!res.ok) {
-        throw new Error(data.message || "Something went wrong");
+      if (data.success === false) {
+        dispatch(signInFailure(data));
+        return;
       }
-      navigate("/"); // Redirect to home page
-      // You can redirect the user or show a success message
-    } catch (err) {
-      setError(err.message);
-    } finally {
-      setLoading(false);
+      dispatch(signInSuccess(data));
+      navigate("/");
+    } catch (error) {
+      dispatch(signInFailure(error));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
@@ -53,7 +49,6 @@ const SignIn = () => {
           id="email"
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
-          value={formData.email}
         />
         <input
           type="password"
@@ -61,7 +56,6 @@ const SignIn = () => {
           id="password"
           className="bg-slate-100 p-3 rounded-lg"
           onChange={handleChange}
-          value={formData.password}
         />
         <button
           disabled={loading}
@@ -70,17 +64,15 @@ const SignIn = () => {
           {loading ? "Loading..." : "Sign In"}
         </button>
       </form>
-      {error && <p className="text-red-500 text-center mt-3">{error}</p>}{" "}
-      {/* Display error message */}
       <div className="flex gap-2 mt-5">
         <p>Dont Have an account?</p>
         <Link to="/sign-up">
           <span className="text-blue-500">Sign up</span>
         </Link>
       </div>
-      <p className="text-red-700 mt-5">{error && "Something went wrong"}</p>
+      <p className="text-red-700 mt-5">
+        {error ? error.message || "Something went wrong!" : ""}
+      </p>
     </div>
   );
-};
-
-export default SignIn;
+}
